fdd67ffc4c15ccdde6a5075425340254c62f7d54
Ania Kutsa <60103129+AdorableUnicorn@users.noreply.github.com>
2024-10-29 18:19:58 +0100

    Setup gh-pages and make minor adjustments of the app for compatibility with GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "./style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -247,5 +248,10 @@ class Clock extends React.Component {
   }
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Clock />);
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter basename="/react-pomodoro">
+    <Clock />
+  </BrowserRouter>
+);
